e88c9aee630a72723bb92cfc4ccb1a1462bd5706
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-11-08 01:29:26 +0600

    navigation menu

---
+++ src/components/user/Login.js
@@ -0,0 +1,14 @@
+import Layout from "../Layout"
+
+const Login = () => {
+    return (
+        <div>
+            <Layout title="Login Page" className="container col-md-8 offset-md-2">
+                <h4>Login here</h4>
+                <hr />
+            </Layout>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/components/user/Register.js
@@ -0,0 +1,16 @@
+import Layout from "../Layout"
+
+
+
+const Register = () => {
+    return (
+        <div>
+            <Layout title="Registration Page" className="container col-md-8 offset-md-2">
+                <h4>Registration here</h4>
+                <hr />
+            </Layout>
+        </div>
+    )
+}
+
+export default Register
