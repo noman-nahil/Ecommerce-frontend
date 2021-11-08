cb3ed7774f5031de2a4fe7fab848dad5aac8c802
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-11-08 17:29:31 +0600

    user sign out

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,6 @@
 import { Link, withRouter } from 'react-router-dom';
+import { singOut, isAuthenticate } from '../utils/auth';
+
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) {
@@ -11,17 +13,29 @@ const isActive = (history, path) => {
 
 const Menu = ({ history }) => {
     return (
-        <nav className='navbar navbar-dark bg-dark'>
+        <nav className='navbar navbar-light bg-light'>
             <ul className="nav nav-tabs" >
                 <li className="nav-item">
                     <Link className="nav-link" to="/" style={isActive(history, '/')}>Home</Link>
                 </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/login" style={isActive(history, '/login')}>Login</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/register" style={isActive(history, '/register')}>Register</Link>
-                </li>
+                {!isAuthenticate() && (<>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login" style={isActive(history, '/login')}>Login</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/register" style={isActive(history, '/register')}>Register</Link>
+                    </li>
+                </>)}
+                {isAuthenticate() && (<>
+                    <li className="nav-item">
+                        <span className="nav-link" style={{ cursor: 'pointer', color: 'grey' }} onClick={() => {
+                            singOut(() => {
+                                history.push('/login');
+                            });
+                        }}>Logout</span>
+                    </li>
+                </>)}
+
             </ul>
         </nav>
     )
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -10,7 +10,14 @@ export const isAuthenticate = () => {
     if (typeof window === 'undefined') return false;
     if (localStorage.getItem('jwt')) {
         const { exp } = jwt_decode(JSON.parse(localStorage.getItem('jwt')));
-        return new Date().getTime() < exp * 1000;
+        if (new Date().getTime() < exp * 1000) {
+            return true;
+        }
+        else {
+            localStorage.removeItem('jwt');
+            return false
+        }
+
     }
     else return false
 }
